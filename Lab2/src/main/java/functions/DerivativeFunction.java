60bebbc2e8bed831a746da08a93397157a8886d0
Maksim <maks4568jk@gmail.com>
2025-09-27 15:43:16 +0500

    DerivativeFunction

---
+++ Lab2/src/main/java/functions/DerivativeFunction.java
@@ -0,0 +1,21 @@
+package functions;
+
+public class DerivativeFunction implements  MathFunction{
+    private  MathFunction originalFunction;
+    private double deltaX;
+
+    public  DerivativeFunction(MathFunction originalFunction, double deltaX)
+    {
+        this.originalFunction = originalFunction;
+        this.deltaX = deltaX;
+    }
+
+    public DerivativeFunction(MathFunction originalFunction) {
+        this(originalFunction, 1e-9); // Значение по умолчанию
+    }
+
+    @Override
+    public double apply(double x) {
+        return (originalFunction.apply(x+deltaX)-originalFunction.apply(x))/deltaX;
+    }
+}
