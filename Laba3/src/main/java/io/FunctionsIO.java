a896b275746ef9f5b73de031ded559dd4e814c88
Александр Бураков <aleksandr2006qwe@mail.ru>
2025-10-12 15:39:01 +0400

    Обновил FunctionsIO и добавил класс TabulatedFunctionFileWriter

--- Laba3/src/main/java/io/FunctionsIO.java
+++ Laba3/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,26 @@
 package io;
 
+import functions.Point;
+import functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Невозможно создать экземпляр служебного класса");
     }
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+        PrintWriter printWriter = new PrintWriter(writer);
+
+        printWriter.println(function.getCount());
+
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+
+        printWriter.flush();
+    }
+
 }
