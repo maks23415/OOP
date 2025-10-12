b56b1a5423aaa2f2de4d5ffd4b2901e1eb18887d
Maksim <maks4568jk@gmail.com>
2025-10-12 19:04:15 +0500

    добавил TabulatedFunctionFileInputStream

--- Laba3/src/main/java/io/FunctionsIO.java
+++ Laba3/src/main/java/io/FunctionsIO.java
@@ -84,4 +84,22 @@ public final class FunctionsIO {
         }
     }
 
+    public static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+        int count = dataInputStream.readInt();
+
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+
+        for (int i = 0; i < count; i++) {
+            xValues[i] = dataInputStream.readDouble();
+            yValues[i] = dataInputStream.readDouble();
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
+
+
 }
