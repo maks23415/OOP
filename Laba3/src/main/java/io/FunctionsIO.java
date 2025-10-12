a9271cb8d78a52dbd1bc664482625617f365e08a
Александр Бураков <aleksandr2006qwe@mail.ru>
2025-10-12 15:10:38 +0400

    Добавил завершённый класс FunctionsIO

---
+++ Laba3/src/main/java/io/FunctionsIO.java
@@ -0,0 +1,7 @@
+package io;
+
+public final class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Невозможно создать экземпляр служебного класса");
+    }
+}
