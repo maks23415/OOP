b884cd0aa99f0f24bf1874d62da9d59c17c4a438
Александр Бураков <aleksandr2006qwe@mail.ru>
2025-10-26 12:44:07 +0400

    Добавил класс MultiplyingTask

---
+++ Lab4/src/main/java/concurrent/MultiplyingTask.java
@@ -0,0 +1,21 @@
+package concurrent;
+
+import functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable{
+    private final TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function) {
+        this.function = function;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            synchronized (function) {
+                double currentY = function.getY(i);
+                function.setY(i, currentY * 2);
+            }
+        }
+        System.out.println("Поток " + Thread.currentThread().getName() + " завершил выполнение MultiplyingTask");
+    }
+}
